4e24a27091a0815603734f07615b294ddb1705c7
azikrasalma <azikrasalma352@gmail.com>
2021-02-11 18:11:05 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Azikra Salma
+203040016
+https://github.com/azikrasalma/pw2021_203040016
+pertemuan 2 - 11 februari 2021
+Mempelajari mengenai sintaks PHP
+*/
+?>
+<?php 
+// Standar Ouput 
+// echo, print
+// print_r
+// var-dumb
+
+// Penulisan sintaks PHP
+// 1.PHP didalam HTML
+// 2.HTML didalam PHP
+
+// Variabel dan Tipe Data
+// Varibel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $Nama = "Azikra Salma";
+// echo'Hallo, Nama Saya $Nama';
+
+// Operator
+// aritmatika
+// + - * / %
+// $x= 10;
+// $y= 20;
+// echo $x * $y;
+
+// penggabung string / concatenation / concat
+// .
+// $Nama_Depan = "Azikra Salma";
+// $Nama_Belakang = "Sabilla";
+// echo $Nama_Depan . " " . $Nama_Belakang;
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -=5;
+// echo $x;
+// $Nama = "Azikra Salma";
+// $Nama.= "";
+// $Nama.= "Sabilla";
+// echo $Nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+//  Identitas
+// ===, !==
+// var_dump(1 === "1");
+
+
+// Logika
+// &&, ||, !
+// $x = 30;
+// var_dump($x < 20 || $x % 2 ==0);
+
+?>
+
+
+<!-<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Hallo, Selamat Datang <?php echo "Azikra";?></h1>
+</body>
+</html>
