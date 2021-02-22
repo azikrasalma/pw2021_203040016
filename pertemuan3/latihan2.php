0a5e6928d9976ffaee2c0c2d069d8d2c7469d6bb
azikrasalma <azikrasalma352@gmail.com>
2021-02-22 15:13:44 +0700

    menyelesaikan video pertemuan 3

---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,17 @@
+<?php 
+// pengkondisikan / percabangan
+// if else
+// if else if else
+// ternary
+// switch
+	
+$x = 2;
+if ($x < 20) {
+	echo "Benar";
+} else if( $x == 20) { 
+    echo "bingo!";
+} else {
+    echo "salah";
+}
+
+?>
