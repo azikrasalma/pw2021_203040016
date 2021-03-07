baf17e2548e69e0c70dce146ecd89cc3e7481b2b
azikrasalma <azikrasalma352@gmail.com>
2021-03-07 10:11:15 +0700

    menyelesaikan tugas 1 praktikum

---
+++ praktikum/tugas1/latihan1a.php
@@ -0,0 +1,13 @@
+<!--
+    AZIKRA SALMA SABILLA
+    203040016
+-->
+<?php
+
+for($i = 1; $i <= 3; $i++) {
+    for ($j = 1; $j <= 3; $j++) {
+        echo "perulangan ke ($i,$j) <br>";
+    }
+}
+
+?>
